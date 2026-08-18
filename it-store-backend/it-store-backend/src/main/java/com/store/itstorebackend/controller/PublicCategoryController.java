1c842ca6b0073afdd6b7ec8166b9a78514a3b457
Adil Nouri <adilniuri2000@gmail.com>
2026-08-18 16:59:29 +0100

    checkpoint before frontend architecture refactoring

--- it-store-backend/it-store-backend/src/main/java/com/store/itstorebackend/controller/PublicCategoryController.java
+++ it-store-backend/it-store-backend/src/main/java/com/store/itstorebackend/controller/PublicCategoryController.java
@@ -1,4 +1,22 @@
 package com.store.itstorebackend.controller;
 
+import com.store.itstorebackend.entity.Category;
+import com.store.itstorebackend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/public/categories")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PublicCategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
 }
